d891d93fd7c0242b509b90bf1fbf704a837b98a5
senalransika <49312938+senalransika@users.noreply.github.com>
2020-12-13 17:55:34 +0530

    keels user profile

---
+++ src/pages/UserProfile/component/keelsuserprofile/keelsuserprofile.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function profile(){
+    return(
+        <div>
+            <h1>KEELS USER-PROFILE</h1>
+            <form>
+                <fieldset>
+                    <lable>
+                        <p>Profile Image</p>
+                        <input type="file" accept="image/*" multiple="fales" />
+                    </lable>
+                    <label>
+                        <p>First Name</p>
+                        <input name="firstname"/>
+                    </label>
+                    <lable>
+                        <p>Last Name</p>
+                        <input name="lastname"/>
+                    </lable>
+                    <lable>
+                        <p>Contact Number</p>
+                        <input contact="contact" />
+                    </lable>
+                    <lable>
+                        <p>Email</p>
+                        <input email="email" />
+                    </lable>
+                    <label>
+                        <p>ID</p>
+                        <input id="id" />
+                    </label>
+                    <label>
+                        <p>POSITION</p>
+                        <input position="position" />
+                    </label>
+                    <label>
+                        <p>BRANCH</p>
+                        <input branch="branch" />
+                    </label>
+                 </fieldset>
+                <button type="submit">Submit</button>
+                <button type="reset">reset</button>
+                </form> 
+        </div>
+    );
+}
+export default profile;
---
+++ src/sayhello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProfilePage = (props) => (
+import Profile from './component/keelsuserprofile/keeluserProfile';
+const ProfilePage = () => (
+  <div>
+    <h1>Profile</h1>
+    <Profile />
+  </div>
+);
