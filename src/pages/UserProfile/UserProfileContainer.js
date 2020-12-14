5404630ad82afc5ec5ccd0b9a962cd29c064cf20
SGeeshan <59595735+SGeeshan@users.noreply.github.com>
2020-12-14 15:08:46 +0530

    Farmerprofile update

--- src/pages/UserProfile/UserProfileContainer.js
+++ src/pages/UserProfile/UserProfileContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from './component/Departmentuserprofile/DepartmentUserProfile';
 import Profilek from './component/keelsuserprofile/keelsuserprofile';
+import ProfileF from './component/Farmeruserprofile/FarmerUserProfile';
 const ProfilePage = () => (
   <div>
     <h1>Profile</h1>
     <Profile />
     <Profilek />
+    <ProfileF />
   </div>
 );
 
