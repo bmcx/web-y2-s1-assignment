1eea17edf23eebdef68006e682a14e673adc7cbc
senalransika <49312938+senalransika@users.noreply.github.com>
2020-12-15 12:20:29 +0530

    add in to container the new file

--- src/pages/UserProfile/UserProfileContainer.js
+++ src/pages/UserProfile/UserProfileContainer.js
@@ -2,12 +2,14 @@ import React from "react";
 import Profile from './component/Departmentuserprofile/DepartmentUserProfile';
 import Profilek from './component/keelsuserprofile/keelsuserprofile';
 import ProfileF from './component/Farmeruserprofile/FarmerUserProfile';
+import Profilek from './component/keelsshowdata/keelsshowdata';
 const ProfilePage = () => (
   <div>
     <h1>Profile</h1>
     <Profile />
     <Profilek />
     <ProfileF />
+    <Profilek />
   </div>
 );
 
