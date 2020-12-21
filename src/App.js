0c456e033fa89540e35da1e48114529ff30453ed
Chandima Bandara <bmcbandara@hotmail.com>
2020-12-21 23:47:47 +0530

    added react toast package

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePageContainer";
 import ProfilePage from "./pages/UserProfile/UserProfileContainer";
 import SideNav from "./common/containers/SideNavContainer";
+import AuthContainer from "./pages/Auth/AuthContainer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer position="top-center" toastClassName="rounded-lg"/>
+      {/* <AuthContainer /> */}
       <div className="w-screen h-screen p-2 bg-gray-50 flex">
         <SideNav />
         <div className="flex-1 overflow-y-auto">
