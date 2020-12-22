95ed8baf24d9372bfb461a43949c193d4e0de3f2
Chandima Bandara <bmcbandara@hotmail.com>
2020-12-22 23:15:55 +0530

    moved google sign in button to new file

--- src/pages/Auth/component/SignInComponent.js
+++ src/pages/Auth/component/SignInComponent.js
@@ -4,7 +4,10 @@ import { NavLink } from "react-router-dom";
 import FormInput from "../../../common/components/FormInput";
 import FormInputPassword from "../../../common/components/FormInputPassword";
 import { IconSpinner } from "../../../common/components/Icons";
+import LogoLong from "../../../common/components/LogoLong";
+import SignInWithGoogleButton from "../../../common/components/SignInWithGoogleButton";
 import {
+  hideAuthModal,
   signInAction,
   signInWithGoogleAction,
 } from "../../../state/auth/authActions";
@@ -32,35 +35,15 @@ const SignInForm = (props) => {
 
   return (
     <div className="bg-gray-50 rounded-lg w-96 shadow-lg p-4 z-20">
-      <h2 className="text-2xl font-black text-gray-700 text-center">Harvest</h2>
-
-      <p className="text-xl text-gray-600 text-center">Welcome back!</p>
+      <LogoLong />
       <button
-        onClick={() => props.signInWithGoogle()}
-        className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100 w-full transition-all ease-out duration-400 focus:outline-none"
+        onClick={() => props.hideAuthModal()}
+        className="absolute bg-gray-100 w-8 h-8 top-0 right-0 rounded-lg flex items-center hover:shadow-md justify-center transition-all ease-out duration-400 focus:outline-none"
       >
-        <div className="px-4 py-3 text-center text-gray-600 font-bold w-full">
-          <svg className="h-6 w-6 absolute" viewBox="0 0 40 40">
-            <path
-              d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-              fill="#FFC107"
-            />
-            <path
-              d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-              fill="#FF3D00"
-            />
-            <path
-              d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-              fill="#4CAF50"
-            />
-            <path
-              d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-              fill="#1976D2"
-            />
-          </svg>
-          Continue with Google
-        </div>
+        🗙
       </button>
+      <p className="text-xl text-gray-600 text-center">Welcome back!</p>
+      <SignInWithGoogleButton onClick={() => props.signInWithGoogle()} />
       <div className="mt-4 flex items-center justify-between">
         <span className="border-b w-1/5 lg:w-1/4"></span>
 
@@ -125,14 +108,16 @@ const SignInForm = (props) => {
 
       <div className="mt-4 flex items-center justify-between">
         <span className="border-b w-1/5 md:w-1/4"></span>
-      
-          <button
-            onClick={()=>{setPage("SignUp")}}
-            disabled={loading}
-            className="text-xs text-gray-500 uppercase hover:underline focus:outline-none"
-          >
-            or sign up
-          </button>
+
+        <button
+          onClick={() => {
+            setPage("SignUp");
+          }}
+          disabled={loading}
+          className="text-xs text-gray-500 uppercase hover:underline focus:outline-none"
+        >
+          or sign up
+        </button>
 
         <span className="border-b w-1/5 md:w-1/4"></span>
       </div>
@@ -149,6 +134,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     signIn: (data) => dispatch(signInAction(data)),
     signInWithGoogle: () => dispatch(signInWithGoogleAction()),
+    hideAuthModal: () => dispatch(hideAuthModal()),
   };
 };
 
