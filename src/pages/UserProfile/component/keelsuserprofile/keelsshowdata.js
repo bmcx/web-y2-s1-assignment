7d280be20a408f7b9e4666e989ee6a0d3c103a25
senalransika <49312938+senalransika@users.noreply.github.com>
2020-12-28 16:49:08 +0530

    keels show data

--- src/pages/UserProfile/component/keelsuserprofile/keelsshowdata.js
+++ src/pages/UserProfile/component/keelsuserprofile/keelsshowdata.js
@@ -1,61 +1,195 @@
-import React from "react";
 
-function Profilen() {
-  return (
-    <div>
-      <h1>
-        <b>Profile</b>
-      </h1>
-      <img
-        src="img_girl.jpg"
-        alt="Girl in a jacket"
-        style="width:50px;height:60px;"
-      />
-      <p>User name:Kamal</p>
-      <p>Character:Keels staff</p>
-      <table>
-        <tbody>
-          <tr>
-            <td>Name:</td>
-            <td>Smith</td>
-          </tr>
-          <tr>
-            <td>Contact Number:</td>
-            <td>+94xxxxxxxxxx</td>
-          </tr>
-          <tr>
-            <td>E-mail:</td>
-            <td>keels@gmail.com</td>
-          </tr>
-          <tr>
-            <td>Id:</td>
-            <td>12345</td>
-          </tr>
-          <tr>
-            <td>Position:</td>
-            <td>chairman</td>
-          </tr>
-          <tr>
-            <td>Branch:</td>
-            <td>Kurunegala</td>
-          </tr>
-        </tbody>
-      </table>
-      <p>
-        Date/Time: <span id="datetime"></span>
-      </p>
+import React, { Component } from "react";
 
-      <p>Date/Time:</p>
-      <p id="datetime"></p>
-      <p>
-        Date/Time: <span id="datetime"></span>
-      </p>
 
-      <script>
-        var dt = new Date(); document.getElementById("datetime").innerHTML =
-        dt.toLocaleString();
-      </script>
-    </div>
-  );
+class keelsshowdata extends Component {
+  render() {
+    return (
+      <div className="h-full">
+        <div className="flex bg-green-200 w-full h-60  rounded-lg">
+          <div className="bottom-0 self-end flex flex-row ">
+            <div className="bg-gray-400 w-32 h-32 m-5 rounded-lg shadow-2xl ml-16 mt-10 absolute">{/* profile picture */}</div>
+            <div className="flex flex-col mt-10 leading-5 text-sm absolute ml-56	">
+              <div>Mr.R.k.S.Ransika</div>
+              <div>Keels Stock Manager,keels(pvt)Ltd</div>
+              <div>srnsika@gmail.com</div>
+              <div>23434490v</div>
+            </div>
+            <div className="bg-gray-200 w-full h-32  rounded-lg shadow-lg ">{/* profile picture */}</div>
+      <div class="w-full relative mt-4 shadow-2xl rounded my-24 overflow-hidden">
+        <div class="top h-64 w-full bg-blue-600 rounded overflow-hidden relative">
+          <img
+            src="https://images.unsplash.com/photo-1542223189-67a03fa0f0bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"
+            alt=""
+            class="bg w-full h-full rounded object-cover object-center absolute z-0"
+          />
+          <div class="flex flex-col justify-center items-right relative h-full bg-black bg-opacity-50 text-white">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+              class="h-24 w-24 object-cover object-center rounded"
+            />
+            <h1 class="text-2xl font-semibold">Senal Ransika</h1>
+            <h4 class="text-sm font-semibold">KEELS ID-224560</h4>
+          </div>
+        </div>
+        <div className="bg-blue h-100 w-full mt-16">
+          <h2>Description</h2>
+            <div className="bg-gray-200 w-full h-32  rounded-lg shadow-lg  mt-4"></div>
+            
+        </div>
+        <div>
+          <h2>anything</h2>
+          <div className="bg-gray-200 w-full h-32  rounded-lg shadow-lg  mt-4"></div> 
+        </div>
+        <div>
+          <h2>reviews</h2>
+          <div className="bg-gray-200 w-full h-32  rounded-lg shadow-lg  mt-4"></div> 
+        <div class="grid grid-cols-12 bg-white">
+          <div class="col-span-12 w-full px-3 py-6 justify-center flex space-x-4 border-b border-solid md:space-x-0 md:space-y-4 md:flex-col md:col-span-2 md:justify-start ">
+            <a
+              href="#"
+              class="text-sm p-2 bg-green-600 text-white text-center rounded font-bold"
+            >
+              Keels Employee Information
+            </a>
+
+            <a
+              href="#"
+              class="text-sm p-2 bg-green-600 text-white text-center rounded font-semibold"
+            >
+              Edit
+            </a>
+            <div>
+                  <h3 class="text-xl font-semibold">Help Links</h3>
+                  <hr />
+                </div>
+
+            <div class="form-item">
+                  <label class="text-l ">Department of Agriculture</label>
+                  <input
+                    type="text"
+                    value="https://Departmentofagriculture.com/"
+                    class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
+                    disabled
+                  />
+                </div>
+
+            <div class="form-item">
+                  <label class="text-l ">Keels(pvt)Ltd</label>
+                  <input
+                    type="text"
+                    value="https://www.keellssuper.com/welcome/"
+                    class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
+                    disabled
+                  />
+                </div>
+
+            <div class="form-item">
+                  <label class="text-l ">Help</label>
+                  <input
+                    type="text"
+                    value="https://Zeis.com/"
+                    class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
+                    disabled
+                  />
+                </div>
+          </div>
+
+          <div class="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
+            <div class="px-4 pt-4">
+              <form action="#" class="flex flex-col space-y-8">
+                <div>
+                  <h3 class="text-2xl font-semibold"> Keels Employee Information</h3>
+                  <hr />
+                </div>
+
+                <div class="form-item">
+                  <label class="text-xl ">Full Name</label>
+                  <input
+                    type="text"
+                    value="Senal Ransika"
+                    class="w-full appearance-none text-black text-opacity-40 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+                    disabled
+                  />
+                </div>
+                <div class="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4">
+                <div class="form-item w-full">
+                  <label class="text-xl ">Username</label>
+                  <input
+                    type="text"
+                    value="Senal03"
+                    class="w-full appearance-none text-black text-opacity-40 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+                    disabled
+                  />
+                </div>
+                <div class="form-item w-full">
+                  <label class="text-xl ">ID</label>
+                  <input
+                    type="text"
+                    value="000000091"
+                    class="w-full appearance-none text-black text-opacity-40 rounded shadow py-1 px-2 mr-2 focus:outliine-none focus:shadow-outline focus:border-blue-200 text-opacity-25"
+                    disabled
+                  />
+                </div>
+                </div>
+
+                <div class="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4">
+  
+                  <div class="form-item w-full">
+                    <label class="text-xl ">Email</label>
+                    <input
+                      type="text"
+                      value="srnsika@gmail.com"
+                      class="w-full appearance-none text-black text-opacity-40 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
+                      disabled
+                    />
+                  </div>
+                  <div class="form-item w-full">
+                    <label class="text-xl ">Contact</label>
+                    <input
+                      type="text"
+                      value="000-0000000"
+                      class="w-full appearance-none text-black text-opacity-40 rounded shadow py-1 px-2 mr-2 forcus:outline-none focus:shadow-outline focus:border-blue-200 text-opscity-25"
+                      disabled
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <h3 class="text-2xl font-semibold ">Rating & farmer</h3>
+                  <hr />
+                </div>
+
+                <div class="form-item w-full">
+                  <label class="text-xl ">Rates</label>
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
+                    disabled
+                  >
+                    
+                  </textarea>
+                </div>
+                <div class="form-item w-full">
+                  <lable class="text-xl ">Available Stock</lable>
+                  <textarea
+                  cols="30"
+                  rows="10"
+                  class="w-full apearance-none text=black text-opacity-50 roounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+                  disabled
+                  >
+
+                  </textarea>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default Profilen;
+
+
+export default keelsshowdata;
