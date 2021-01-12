c83ed7d01e24c1f800b267106ca38f74badb87b9
SGeeshan <59595735+SGeeshan@users.noreply.github.com>
2021-01-13 02:29:48 +0530

    graph

--- src/pages/GraphPage/GraphPageContainer.js
+++ src/pages/GraphPage/GraphPageContainer.js
@@ -1,8 +1,144 @@
 const GraphPageContainer = () => {
     return (
         <div>
-            Graphs
+            
+<div class="flex items- min-h-screen bg-gray-200 text-gray-800">
+  <div class="p-4 w-full">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12 sm:col-span-6 md:col-span-3">
+        <div class="flex flex-row bg-white shadow-sm rounded p-4">
+          <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-blue-100 text-blue-500">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+          </div>
+          <div class="flex flex-col flex-grow ml-4">
+            <div class="text-sm text-gray-500">Users</div>
+            <div class="font-bold text-lg">1259</div>
+          </div>
         </div>
+      </div>
+      <div class="col-span-12 sm:col-span-6 md:col-span-3">
+        <div class="flex flex-row bg-white shadow-sm rounded p-4">
+          <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-green-100 text-green-500">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+          </div>
+          <div class="flex flex-col flex-grow ml-4">
+            <div class="text-sm text-gray-500">Orders</div>
+            <div class="font-bold text-lg">230</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-12 sm:col-span-6 md:col-span-3">
+        <div class="flex flex-row bg-white shadow-sm rounded p-4">
+          <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-orange-100 text-orange-500">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+          </div>
+          <div class="flex flex-col flex-grow ml-4">
+            <div class="text-sm text-gray-500">New Clients</div>
+            <div class="font-bold text-lg">190</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-12 sm:col-span-6 md:col-span-3">
+        <div class="flex flex-row bg-white shadow-sm rounded p-4">
+          <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-red-100 text-red-500">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          </div>
+          <div class="flex flex-col flex-grow ml-4">
+            <div class="text-sm text-gray-500">Revenue</div>
+            <div class="font-bold text-lg">$ 32k</div>
+          </div>
+        </div>
+    </div>
+    </div>
+        
+        <div class="w-full max-w-screen-xl mx- px-">
+          <div class="flex justify-left p- px- py-5">
+            <div class="w-full max-w-md">
+                <div class="bg-white shadow-md rounded-lg px-3 py-2 mb-4">
+                    <div class="block text-gray-700 text-lg font-semibold py-2 px-2">
+                        Item List
+                    </div>
+                    <div class="flex items-center bg-gray-200 rounded-md">
+                        <div class="pl-2">
+                            <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path class="heroicon-ui"
+                                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                            </svg>
+                        </div>
+                        <input
+                            class="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+                            id="search" type="text" placeholder="Search Items"></input>
+                    </div>
+                    <div class="py-3 text-sm">
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">carrot</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Dabulla</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Tomato</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Diyathalwa</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">orange</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Bandarawela</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">onions</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Anuradapura</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Bananas</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Polonnaruwa</div>
+                        </div>
+                    </div>
+                    <div class="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">
+                        <button class="hover:text-gray-600 text-gray-500 font-bold py-2 px-4">
+                            Cancel
+                        </button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="grid gap-6 mb-8 md:grid-cols-2">  
+    <div class="min-w-0 p-4 text-Black bg-white rounded-lg shadow-xs">
+                <h4 class="mb-4 font-semibold">
+                  Chart
+                </h4>
+                <p>
+
+
+
+
+
+
+
+                  
+                </p>
+              </div>
+            </div>
+            
+    </div>        
+    </div>
+      </div>
+    </div>
+  
+
+            
+
+         
+    
+
+		
+          
     )
 }
 
